fa53833e85d03e29f20a210f091f76f0627c1949
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2024-01-28 21:24:19 +0000

    ALBNSQR-8 Discord Sign-In and Sign-Up

--- packages/svelte-app/src/routes/auth/+page.svelte
+++ packages/svelte-app/src/routes/auth/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import { Button } from "flowbite-svelte";
 	import { signIn, signOut } from "@auth/sveltekit/client";
-	import { page } from "$app/stores";
 </script>
 
 <Button on:click={() => signIn("discord", { callbackUrl: "/app" })}>Sign-In with Discord</Button>
