79faeee6349b1644734a09545b36b301fc013072
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2024-03-26 02:49:40 +0000

    ALBNSQR-8 Project reset

---
+++ packages/svelte-app/src/routes/app/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import AppNavbar from '$lib/app-navbar.svelte';
+	import AppSidebar from '$lib/app-sidebar.svelte';
+</script>
+
+<AppNavbar />
+<AppSidebar />
+
+<main class="p-8 pt-0 lg:ml-[15rem] 2xl:ml-[19rem]">
+	<slot />
+</main>
