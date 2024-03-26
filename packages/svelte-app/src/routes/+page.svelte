7fea1e638d82cec26f7c3669921e704a30df72ae
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2024-03-26 02:20:10 +0000

    ALBNSQR-8 Project reset

--- packages/svelte-app/src/routes/+page.svelte
+++ packages/svelte-app/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import HomeNavbar from '$lib/home-navbar.svelte';
+</script>
+
+<HomeNavbar />
+
+<div
+	class="hero min-h-screen"
+	style="background-image: url(/albion-online-art-1.png);"
+>
+	<div class="hero-overlay bg-opacity-60"></div>
+	<div class="hero-content text-center text-neutral-content">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold capitalize">Albion Online Companion App</h1>
+			<p class="mb-5">
+				Find the perfect guild, share your layouts and search the markets
+			</p>
+			<button class="btn btn-primary">Get Started</button>
+		</div>
+	</div>
+</div>
---
+++ packages/svelte-app/src/routes/app/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import AppNavbar from "$lib/app-navbar.svelte";
+    import AppSidebar from "$lib/app-sidebar.svelte";
+</script>
+
+<AppNavbar/>
+<AppSidebar/>
